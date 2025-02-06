2b9b212f83ed06e7c24e795185f87ca949ac4865
EmberHeart <130223525+LittleDuck-LD@users.noreply.github.com>
2025-02-06 22:34:32 +0800

    更正api断点

--- src/data/api.js
+++ src/data/api.js
@@ -6,9 +6,9 @@ export default {
         { name: 'bedphp', ico: 'https://q.qlogo.cn/g?b=qq&s=640&nk=403376273' },
     ],
     api: {
-        default_api: 'http://192.168.0.100:3000/api/',//'https://8.140.25.35:13000/v1/',
-        player_api: 'http://192.168.0.100:3000/api/player/',
-        club_api: 'http://192.168.0.100:3000/api/club/',
+        default_api: 'https://api.brawlstars.top/api/',//'https://8.140.25.35:13000/v1/',
+        player_api: 'https://api.brawlstars.top/api/player/',
+        club_api: 'https://api.brawlstars.top/api/club/',
         roomloddy_api: 'https://8.140.25.35:3005/',
         hero_images_api: 'https://bitbucket.org/bedphp/bsimages2/raw/bf1a1c0929ebe2671d9a6f6c093897b65611c0e0/images'
     },
