b9c8c9c4f304a26cc3e3c98c1986153c7c3bb5af
bedphp <bedphp@outlook.com>
2025-02-06 23:42:23 +0800

    修复夜间模式，临时修改api.js

--- src/data/api.js
+++ src/data/api.js
@@ -7,8 +7,8 @@ export default {
     ],
     api: {
         default_api: 'http://192.168.0.100:3000/api/',//'https://8.140.25.35:13000/v1/',
-        player_api: 'http://192.168.0.100:3000/api/player/',
-        club_api: 'http://192.168.0.100:3000/api/club/',
+        player_api: 'http://api.brawlstars.top/api/player/',
+        club_api: 'http://api.brawlstars.top/api/club/',
         roomloddy_api: 'https://8.140.25.35:3005/',
         hero_images_api: 'https://bitbucket.org/bedphp/bsimages2/raw/bf1a1c0929ebe2671d9a6f6c093897b65611c0e0/images'
     },
